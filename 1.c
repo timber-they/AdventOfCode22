8997f445e964e83173b80ef822bbb0cd1387b361
User Private <user@Private.com>
2022-12-01 17:44:54 +0100

    1 2

--- 1.c
+++ 1.c
@@ -54,8 +54,47 @@ int part1(FILE *in)
 
 int part2(FILE *in)
 {
-    if (in == NULL)
-        return -1;
-    return -2;
+    int c;
+    int num = 0;
+    int sum = 0;
+    int max1 = 0;
+    int max2 = 0;
+    int max3 = 0;
+    while ((c = fgetc(in)) != EOF)
+    {
+        if (c >= '0' && c <= '9')
+        {
+            num = num * 10 + (c - '0');
+            continue;
+        }
+        if (c != '\n')
+        {
+            fprintf(stderr, "Unexpected character: '%c'\n", c);
+            exit(1);
+        }
+        if (num != 0)
+        {
+            // End of number
+            sum += num;
+            num = 0;
+            continue;
+        }
+        // End of block, bc two line breaks in a row
+        if (sum > max1)
+        {
+            max3 = max2;
+            max2 = max1;
+            max1 = sum;
+        }
+        else if (sum > max2)
+        {
+            max3 = max2;
+            max2 = sum;
+        }
+        else if (sum > max3)
+            max3 = sum;
+        sum = 0;
+    }
+    return max1 + max2 + max3;
 }
 
