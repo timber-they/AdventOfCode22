865d9a912759c007cdeadac083e5818ba7b6aadb
User Private <user@Private.com>
2022-12-15 21:32:41 +0100

    15 2 - a bit ugly

--- 15.c
+++ 15.c
@@ -4,10 +4,10 @@
 #include <errno.h>
 #include <math.h>
 
-#define SENSORS 14//26
+#define SENSORS 26
 
 int part1(FILE *in);
-int part2(FILE *in);
+long part2(FILE *in);
 void read(FILE *in, int *sX, int *sY, int *bX, int *bY);
 void fillDists(int *sX, int *sY, int *bX, int *bY, int *dists);
 void fillRanges(int *sX, int *sY, int *dists, int y, int *starts, int *ends);
@@ -16,14 +16,17 @@ int countInRow(int *sX, int *sY, int *dists, int row);
 // starts and ends should have size count*(count+1)/2
 int distinctRanges(int *starts, int *ends, int count);
 int makeDistinct(int *start, int *end, int oStart, int oEnd, int *nStart, int *nEnd);
+int removeEmptyIntervals(int *starts, int *ends, int count);
+int countBeacons(int row, int *bX, int *bY);
+int joinTogether(int *starts, int *ends, int count);
 
 int main()
 {
-    FILE *in = fopen("test15", "r");
+    FILE *in = fopen("in15", "r");
 
     printf("Part1: %d\n", part1(in));
     rewind(in);
-    printf("Part2: %d\n", part2(in));
+    printf("Part2: %ld\n", part2(in));
 
     fclose(in);
     return 0;
@@ -35,24 +38,38 @@ int part1(FILE *in)
     read(in, sX, sY, bX, bY);
     int dists[SENSORS];
     fillDists(sX, sY, bX, bY, dists);
-    return countInRow(sX, sY, dists, 10);//2000000);
+    int row = 2000000;
+    return countInRow(sX, sY, dists, row) - countBeacons(row, bX, bY);
 }
 
-int part2(FILE *in)
+long part2(FILE *in)
 {
-    int starts[10], ends[10];
-    starts[0] = 0;
-    starts[1] = -2;
-    starts[2] = 7;
-    starts[3] = 1;
-    ends[0] = 10;
-    ends[1] = -1;
-    ends[2] = 12;
-    ends[3] = 4;
-    int count = distinctRanges(starts, ends, 4);
-    for (int i = 0; i < count; i++)
-        printf("%d-%d\n", starts[i], ends[i]);
-    return in == NULL ? -3 : -2;
+    int sX[SENSORS], sY[SENSORS], bX[SENSORS], bY[SENSORS];
+    read(in, sX, sY, bX, bY);
+    int dists[SENSORS];
+    fillDists(sX, sY, bX, bY, dists);
+    int starts[SENSORS*(SENSORS+1)/2], ends[SENSORS*(SENSORS+1)/2];
+    long max = 4000000;
+    for (int y = 0; y < max; y++)
+    {
+        fillRanges(sX, sY, dists, y, starts, ends);
+        int count = distinctRanges(starts, ends, SENSORS);
+        int i;
+        for (i = 0; i < count; i++)
+        {
+            if (starts[i] <= 0 && ends[i] >= max)
+                goto noBeacon;
+            else if ((starts[i] > 0 && starts[i] < max) || 
+                     (ends[i] < 0 && ends[i] < max))
+                break;
+        }
+        if (starts[i] > 0)
+            return (long)(starts[i]-1) * max + y;
+        else
+            return (long)(ends[i]+1) * max + y;
+noBeacon:;
+    }
+    return -1;
 }
 
 void read(FILE *in, int *sX, int *sY, int *bX, int *bY)
@@ -79,7 +96,6 @@ int countInRow(int *sX, int *sY, int *dists, int row)
     int starts[SENSORS*(SENSORS+1)/2], ends[SENSORS*(SENSORS+1)/2];
     fillRanges(sX, sY, dists, row, starts, ends);
     int count = distinctRanges(starts, ends, SENSORS);
-    printf("Count=%d\n", count);
     for (int i = 0; i < count; i++)
         sum += ends[i]-starts[i]+1;
     return sum;
@@ -98,9 +114,11 @@ void fillRanges(int *sX, int *sY, int *dists, int y, int *starts, int *ends)
 int distinctRanges(int *starts, int *ends, int count)
 {
     int notDistinct = 1;
+    count = removeEmptyIntervals(starts, ends, count);
+    count = joinTogether(starts, ends, count);
+
     while (notDistinct)
     {
-        printf("Got %d ranges\n", count);
         notDistinct = 0;
         int newCount = count;
         for (int i = 0; i < count-1; i++)
@@ -116,19 +134,10 @@ int distinctRanges(int *starts, int *ends, int count)
                     // Got one new interval
                     newCount++;
             }
-        // Remove empty intervals
-        for (int i = 0; i < count; i++)
-            if (starts[i] > ends[i])
-            {
-                for (int j = i; j < newCount-1; j++)
-                {
-                    starts[j] = starts[j+1];
-                    ends[j] = ends[j+1];
-                }
-                newCount--;
-            }
-        count = newCount;
+        count = removeEmptyIntervals(starts, ends, newCount);
+        count = joinTogether(starts, ends, count);
     }
+
     return count;
 }
 
@@ -180,5 +189,66 @@ int makeDistinct(int *start, int *end, int oStart, int oEnd, int *nStart, int *n
     *start = 0;
     *end = -1;
     return 0;
+}
+
+int removeEmptyIntervals(int *starts, int *ends, int count)
+{
+    // Remove empty intervals
+    for (int i = 0; i < count; i++)
+        if (starts[i] > ends[i])
+        {
+            for (int j = i; j < count-1; j++)
+            {
+                starts[j] = starts[j+1];
+                ends[j] = ends[j+1];
+            }
+            count--;
+            i--;
+        }
+    return count;
+}
+
+int joinTogether(int *starts, int *ends, int count)
+{
+    int joined = 1;
+    while (joined)
+    {
+        joined = 0;
+        for (int i = 0; i < count; i++)
+            for (int j = 0; j < i; j++)
+            {
+                if (ends[i] == starts[j]-1)
+                    starts[j] = starts[i];
+                else if (starts[i] == ends[j]+1)
+                    ends[j] = ends[i];
+                else
+                    continue;
+
+                joined = 1;
+                for (int k = i; k < count-1; k++)
+                {
+                    starts[k] = starts[k+1];
+                    ends[k] = ends[k+1];
+                }
+                count--;
+                i--;
+                break;
+            }
+    }
+    return count;
+}
+
+int countBeacons(int row, int *bX, int *bY)
+{
+    int res = 0;
+    for (int i = 0; i < SENSORS; i++)
+    {
+        for (int j = 0; j < i; j++)
+            if (bX[i] == bX[j] && bY[i] == bY[j])
+                goto endOfFor;
+        res += bY[i] == row;
+endOfFor:;
+    }
+    return res;
 }
 
